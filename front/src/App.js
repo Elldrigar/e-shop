237c250efae247fef284d0ab0706356f4f322197
Artur Gawron <artur@gawron.me>
2020-10-16 23:43:55 +0200

    Install and config Prettier, and App.js is now =>

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <h1>Hello!</h1>
-  );
+    <>
+      <h1>Hello!</h1>
+    </>
+  )
 }
 
-export default App;
+export default App
