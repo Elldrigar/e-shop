96069a47e25866e4e7a234aa3c8fc0610b432c80
Artur Gawron <artur@gawron.me>
2020-10-17 22:59:47 +0200

    Add navigation

--- front/src/components/Header/Header.jsx
+++ front/src/components/Header/Header.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 
 const Header = () => {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' callapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/'>E-Shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <Nav.Link href='/cart'>Koszyk</Nav.Link>
+              <Nav.Link href='/login'>Zaloguj</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   )
 }
 
