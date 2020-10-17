ef62a8999fc41e568d65149cf274b1f758bb96a5
Artur Gawron <artur@gawron.me>
2020-10-17 23:06:36 +0200

    Add fontawesome icons

--- front/src/components/Header/Header.jsx
+++ front/src/components/Header/Header.jsx
@@ -10,8 +10,8 @@ const Header = () => {
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-              <Nav.Link href='/cart'>Koszyk</Nav.Link>
-              <Nav.Link href='/login'>Zaloguj</Nav.Link>
+              <Nav.Link href='/cart'><i className='fas fa-shopping-basket' /> Koszyk</Nav.Link>
+              <Nav.Link href='/login'><i className='fas fa-user-alt' /> Zaloguj</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
