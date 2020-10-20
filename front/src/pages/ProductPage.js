58e8ebcb65a30425c4227eccd1f16977610a3b5c
Artur Gawron <artur@gawron.me>
2020-10-20 16:28:36 +0200

    Import bootstrap component, and add go back button

--- front/src/pages/ProductPage.js
+++ front/src/pages/ProductPage.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import Rating from '../components/Rating/Rating'
+import products from '../products'
 
-const ProductPage = () => {
-	return (
-		<div>Product</div>
-	)
+const ProductPage = ({ match }) => {
+  const product = products.find((p) => p._id === match.params.id)
+
+  return (
+    <>
+      <Link to='/' className='btn btn-light my-3'>Wróć</Link>
+    </>
+  )
 }
 export default ProductPage
