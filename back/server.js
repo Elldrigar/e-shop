2bd6c2b5bec199781e499e12f25b1d74157c3fe8
Artur Gawron <artur@gawron.me>
2020-10-22 22:51:13 +0200

    Move prettierrc file, add product/products API

--- back/server.js
+++ back/server.js
@@ -2,7 +2,14 @@ const express = require('express')
 const products = require('./data/products')
 const app = express()
 
-app.get('/', (req,res) => {
-	res.send('API 👍🏻')
+app.get('/', (req, res) => {
+  res.send('API 👍🏻')
+})
+app.get('/products', (req, res) => {
+  res.json(products)
+})
+app.get('/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id)
+  res.json(product)
 })
 app.listen(5555, console.log('Server 🚀 on port 5555'))
