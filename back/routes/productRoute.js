6f20f90a0786f050983c6d20fa5f94464e75f4d9
Artur Gawron <artur@gawron.me>
2020-10-25 21:39:34 +0100

    Use asyncHandler for product and products route

--- back/routes/productRoute.js
+++ back/routes/productRoute.js
@@ -1,14 +1,25 @@
 const express = require('express')
+const asyncHandler = require('express-async-handler')
 const router = express.Router()
 const Product = require('../models/productModel')
 
-router.get('/', async (req, res) => {
-  const products = await Product.find({})
-	res.json(products)
-})
-router.get('/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({})
+    res.json(products)
+  }),
+)
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    if (product) {
+      res.json(product)
+    } else {
+      res.status(404).json({ message: 'Produktu nie znaleziono' })
+    }
+  }),
+)
 
 module.exports = router
