e110e0cf405b487d17aae52e0c0967b76e13f818
Artur Gawron <artur@gawron.me>
2020-10-25 21:56:41 +0100

    Add comments for every route (desc,route and access)

--- back/routes/productRoute.js
+++ back/routes/productRoute.js
@@ -3,6 +3,9 @@ const asyncHandler = require('express-async-handler')
 const router = express.Router()
 const Product = require('../models/productModel')
 
+// @desc       Fetch all products
+// @route      GET /api/products
+// @access     Public
 router.get(
   '/',
   asyncHandler(async (req, res) => {
@@ -10,6 +13,10 @@ router.get(
     res.json(products)
   }),
 )
+
+// @desc       Fetch single product
+// @route      GET /api/products/:id
+// @access     Public
 router.get(
   '/:id',
   asyncHandler(async (req, res) => {
