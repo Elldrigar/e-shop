cc487eaa95ed7f7699e5e87023e417d8a9ade65e
Artur Gawron <artur@gawron.me>
2020-10-26 23:16:06 +0100

    Reorganization

--- back/server.js
+++ back/server.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
+const PORT = process.env.PORT || 5555
 const connectDB = require('./config/db')
 const { notFound, errorHandler } = require('./middleware/errorHandler')
-
 const productRoute = require('./routes/productRoute')
 
 dotenv.config()
-connectDB()
+connectDB().then()
+
+app.listen(PORT, () => {
+  console.log(`Server 🚀 in ${process.env.NODE_ENV} mode on port ${PORT}`)
+})
 
 app.get('/', (req, res) => {
   res.send('API 👍🏻')
@@ -16,10 +20,3 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoute)
 app.use(notFound)
 app.use(errorHandler)
-
-const PORT = process.env.PORT || 5555
-
-app.listen(
-  PORT,
-  console.log(`Server 🚀 in ${process.env.NODE_ENV} mode on port ${PORT}`),
-)
