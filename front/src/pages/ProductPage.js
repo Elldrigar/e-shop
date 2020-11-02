ad7d1b1c70c2b1e9fc16fc0ba9825fed00b30103
Artur Gawron <artur@gawron.me>
2020-11-02 12:43:16 +0100

    Add quantity to product component

--- front/src/pages/ProductPage.js
+++ front/src/pages/ProductPage.js
@@ -1,13 +1,14 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { listProductDetails } from '../actions/productActions'
 import { Link } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
 import { Rating } from '../components/Rating'
 import { Message } from '../components/Message'
 import { Loader } from '../components/Loader'
 
 const ProductPage = ({ match }) => {
+  const [qty, setQty] = useState(0)
   const dispatch = useDispatch()
   const productDetails = useSelector((state) => state.productDetails)
   const { loading, error, product } = productDetails
@@ -61,6 +62,26 @@ const ProductPage = ({ match }) => {
                     <Col>{product.countInStock > 0 ? 'Dostępny' : 'Brak'}</Col>
                   </Row>
                 </ListGroup.Item>
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Ilość</Col>
+                      <Col>
+                        <Form.Control
+                          as='select'
+                          value={qty}
+                          onChange={(e) => setQty(e.target.value)}
+                        >
+                          {[...Array(product.countInStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
                 <ListGroup.Item>
                   <Button
                     className='btn-block'
