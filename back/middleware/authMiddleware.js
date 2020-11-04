f0aeb9c26e234a901275d5c3e7d3595c5812a506
Artur Gawron <artur@gawron.me>
2020-11-04 23:23:55 +0100

    Create decode jwt token

--- back/middleware/authMiddleware.js
+++ back/middleware/authMiddleware.js
@@ -1,10 +1,25 @@
+const asyncHandler = require('express-async-handler')
 const jwt = require('jsonwebtoken')
 const User = require('../models/userModel')
 
-const protect = async (req, res, next) => {
-	let token
-	console.log(req.headers.authorization)
-	next()
-}
+const protect = asyncHandler(async (req, res, next) => {
+  let token
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      token = req.headers.authorization.split(' ')[1]
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+	    console.log(decoded)
+      next()
+    } catch (error) {}
+  }
+  if (!token) {
+    res.status(401)
+    throw new Error('Brak zutoryzacji!')
+  }
+})
 
 module.exports = { protect }
