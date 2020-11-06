6922bcbf60115139df851b5edefeb8a4d5e17c18
Artur Gawron <artur@gawron.me>
2020-11-06 20:56:20 +0100

    Add register user route

--- back/routes/userRoutes.js
+++ back/routes/userRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const {
-	authUser, getUserProfile
-} = require('../controllers/userController')
 const { protect } = require('../middleware/authMiddleware')
+const {
+  authUser,
+  getUserProfile,
+  registerUser,
+} = require('../controllers/userController')
 
-
+router.route('/').post(registerUser)
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile)
 
