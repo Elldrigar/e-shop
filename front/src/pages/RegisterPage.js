0127942d3e61242c4c609616a7e48f54b1dd7e50
Artur Gawron <artur@gawron.me>
2020-11-15 23:57:24 +0100

    Create Register Page

---
+++ front/src/pages/RegisterPage.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from 'react'
+import { FormContainer } from '../components/FormContainer'
+import { Link } from 'react-router-dom'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message/Message'
+import Loader from '../components/Loader/Loader'
+import { register } from '../actions/userActions'
+
+const RegisterPage = ({ location, history }) => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [message, setMessage] = useState(null)
+  const dispatch = useDispatch()
+  const userRegister = useSelector((state) => state.userRegister)
+  const { loading, error, userInfo } = userRegister
+  const redirect = location.search ? location.search.split('=')[1] : '/'
+
+  useEffect(() => {
+    if (userInfo) {
+      history.push(redirect)
+    }
+  }, [history, userInfo, redirect])
+
+  const submitHandler = (event) => {
+    event.preventDefault()
+    //DISPATCH REGISTER
+  }
+
+  return (
+    <FormContainer>
+      <h1>Zarejestruj się</h1>
+      {error && <Message variant='danger'>{error}</Message>}
+      {loading && <Loader />}
+      <Form onSubmit={submitHandler}>
+	      <Form.Group controlId='name'>
+		      <Form.Label>Imię</Form.Label>
+		      <Form.Control
+			      type='name'
+			      placeholder='Wprowadź imię'
+			      value={email}
+			      onChange={(e) => setName(e.target.value)}
+		      />
+	      </Form.Group>
+        <Form.Group controlId='email'>
+          <Form.Label>E-mail</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Wprowadź e-mail'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group controlId='password'>
+          <Form.Label>Hasło</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Wprowadź hasło'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+	      <Form.Group controlId='confirmPassword'>
+		      <Form.Label>Potwierdź Hasło</Form.Label>
+		      <Form.Control
+			      type='password'
+			      placeholder='Potwierdź hasło'
+			      value={confirmPassword}
+			      onChange={(e) => setConfirmPassword(e.target.value)}
+		      />
+	      </Form.Group>
+        <Button type='submit' variant='primary'>
+          Zarejestruj się
+        </Button>
+      </Form>
+      <Row>
+        <Col>
+          Posiadasz już konto?{' '}
+          <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>
+            Zaloguj się
+          </Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  )
+}
+
+export default RegisterPage
