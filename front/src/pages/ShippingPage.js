3278c550b11fb734e489d712f34c3959eccb8b2a
Artur Gawron <artur@gawron.me>
2020-12-03 13:58:25 +0100

    Add shipping address to state

--- front/src/pages/ShippingPage.js
+++ front/src/pages/ShippingPage.js
@@ -2,15 +2,20 @@ import React, { useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from '../components/FormContainer/FormContainer'
+import { saveShippingAdress } from '../actions/cartActions'
 
 const ShippingPage = ({ history }) => {
-  const [address, setAddress] = useState('')
-  const [city, setCity] = useState('')
-  const [postalCode, setPostalCode] = useState('')
-  const [country, setCountry] = useState('')
+  const cart = useSelector((state) => state.cart)
+  const { shippingAddress } = cart
+  const [address, setAddress] = useState(shippingAddress.address)
+  const [city, setCity] = useState(shippingAddress.city)
+  const [postalCode, setPostalCode] = useState(shippingAddress.postalCode)
+  const [country, setCountry] = useState(shippingAddress.country)
+  const dispatch = useDispatch()
   const submitHandler = (event) => {
     event.preventDefault()
-    console.log('button clicked')
+    dispatch(saveShippingAdress({ address, city, postalCode, country }))
+    history.push('/payment')
   }
   return (
     <FormContainer>
