a915e714480f175d899c72c4ebad2bd6b9123023
Artur Gawron <artur@gawron.me>
2021-01-20 15:19:34 +0100

    Add get order by ID controller

--- back/controllers/orderController.js
+++ back/controllers/orderController.js
@@ -2,7 +2,7 @@ const asyncHandler = require('express-async-handler')
 const Order = require('../models/orderModel')
 
 // @desc       Create new order
-// @route      GET /api/orders
+// @route      POST /api/orders
 // @access     Private
 const addOrderItems = asyncHandler(async (req, res) => {
   const {
@@ -21,7 +21,7 @@ const addOrderItems = asyncHandler(async (req, res) => {
   } else {
     const order = new Order({
       orderItems,
-	    user: req.user._id,
+      user: req.user._id,
       shippingAddress,
       paymentMethod,
       itemsPrice,
@@ -30,8 +30,25 @@ const addOrderItems = asyncHandler(async (req, res) => {
       itemsTotal,
     })
 
-	  const createdOrder = await order.save()
-	  res.status(201).json(createdOrder)
+    const createdOrder = await order.save()
+    res.status(201).json(createdOrder)
+  }
+})
+
+// @desc       Get order by ID
+// @route      GET /api/orders/:id
+// @access     Private
+const getOrderbyId = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email',
+  )
+
+  if (order) {
+    res.json(order)
+  } else {
+    res.status(404)
+    throw new Error('Zamówienia nie znaleziono')
   }
 })
 
